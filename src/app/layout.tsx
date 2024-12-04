6edb7eaf8a84449914f6187e3a668e3cb959fa46
Saptadip Saha <74125575+SciMathist@users.noreply.github.com>
2024-12-05 02:37:18 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `AI Safety Arc (AISA) - The AI Safety and Alignment Group`,
+  description: `Website for AI Safety Arc (AISA).`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
